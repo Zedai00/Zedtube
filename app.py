993f6af127addc46a2c5dbead609e3e2bbd73f97
zedgamer9128 <zeeshantechnical9128@gmail.com>
2021-10-18 15:49:48 +0530

    October 18, 2021 3:49 PM

---
+++ app.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from werkzeug.utils import send_from_directory
+import youtube_dl
+from flask import Flask, redirect, render_template, request, send_file,send_from_directory, safe_join, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+        return render_template("index.html")
+
+@app.route("/download", methods=["POST", "GET"])
+def download():
+    if request.method == "GET":
+        return render_template("download.html")
+    else:
+        url = request.form.get("url")
+        ydl_opts = {
+            "cachedir": "False",
+            "ignoreerrors": "True"
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url,])
+            result = ydl.extract_info("{}".format(url))
+            name = ydl.prepare_filename(result)
+            return send_from_directory(app.root_path, name, as_attachment=True)
+
