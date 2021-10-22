92e282a55c4b3ecd8c50bf20b54ae7aba5c130c5
zedgamer9128 <zeeshantechnical9128@gmail.com>
2021-10-22 18:45:13 +0530

    Add Redis

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from __future__ import unicode_literals
-
+import os
+import redis
 from tempfile import mkdtemp
 
 import youtube_dl
-from flask import (
-    Flask,
-    render_template,
-    request,
-    send_file,
-    send_from_directory,
-    session,
-)
+from flask import (Flask, render_template, request, send_file,
+                   send_from_directory, session)
 from flask_session import Session
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_TYPE"] = "redis"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_USE_SIGNER"] = True
+app.config["SESSION_REDIS"] = redis.from_url(os.environ.get("REDIS_SESSION"))
 Session(app)
 
 
